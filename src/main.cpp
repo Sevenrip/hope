10d48831ebac53c4ca4877a108dbf0e1e879def0
Hugo Goncalves <hugo.goncalves@miniclip.com>
2017-07-19 15:22:39 +0000

    fix bugs on main

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 
 using namespace hope;
-struct A {};
+struct A { A(int i) : _i(i) {} int _i;};
 struct B {};
 struct C {};
 
@@ -15,7 +15,8 @@ struct SA : public hope::System {
 int main() {
     hope::SystemsManager manager;
 
-    hope::Entity entity = manager.entities().createEntityWithComponents(A{}, B{});
-    
+    hope::Entity entity = manager.entities().createEntityWithComponents(A{2}, B{}, C{});
+    auto a = entity.component<A>();
+    std::cout << a->_i << std::endl;
     return 0;
 }
